83cb524a7b95509942a9703997150ba890a0a9b0
hadim9637 <hadim9637@HRH-IC0027334.wrdsb.ca>
2018-11-26 08:53:04 -0500

    Testing out GDX

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,50 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+        int x = 0;
+        int y = 0;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+                if (Gdx.input.isKeyPressed(Input.Keys.W)){
+                y++;    
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.A)){
+                x--;
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.S)){
+                y--;
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.D)){
+                x++;
+                }
+                
+                batch.begin();
+		batch.draw(img, x, y);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
