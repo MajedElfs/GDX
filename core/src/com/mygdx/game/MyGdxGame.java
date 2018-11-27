372941fed8f18eb6d90b7f6fb85bb54899426940
hadim9637 <hadim9637@HRH-IC0027334.wrdsb.ca>
2018-11-27 08:58:21 -0500

    Making ponggggggg

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,47 +4,73 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-        int x = 0;
-        int y = 0;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-                if (Gdx.input.isKeyPressed(Input.Keys.W)){
-                y++;    
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.A)){
-                x--;
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.S)){
-                y--;
-                }
-                if (Gdx.input.isKeyPressed(Input.Keys.D)){
-                x++;
-                }
-                
-                batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private SpriteBatch batch;
+    private Paddle player1;
+    private Paddle player2;
+    private Ball ball;
+    private OrthographicCamera cam;
+    private ShapeRenderer shapeBatch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        shapeBatch = new ShapeRenderer();
+        cam = new OrthographicCamera(800, 600);
+        cam.position.x = 400;
+        cam.position.y = 300;
+        cam.update();
+
+        player1 = new Paddle(10, 250, 25, 100, 2);
+        player2 = new Paddle(765, 250, 25, 100, 2);
+        ball = new Ball(390, 290, 20, 20, 2);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            player1.moveUp();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            player1.moveDown();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            player2.moveUp();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            player2.moveDown();
+        }
+        
+        ball.move();
+        if (ball.getTop() > 600 || ball.getBottom() < 0){
+            ball.bounceY();
+        }
+        if (ball.getLeft() < 0 || ball.getRight() > 800){
+            ball.bounceX();
+        }
+       
+
+        shapeBatch.setProjectionMatrix(cam.combined);
+        shapeBatch.begin(ShapeType.Filled);
+        player1.draw(shapeBatch);
+        player2.draw(shapeBatch);
+        ball.draw(shapeBatch);
+        shapeBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+
+    }
 }
